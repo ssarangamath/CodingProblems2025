82361a14dd23a0200f08bfff232d7cf17cf911bb
sachin sarangamath <sachineceuvce@gmail.com>
2025-05-29 14:52:52 +0530

    Adding Subarray problems

--- src/main/java/SubArrayProblems/RemoveShortestSubarray.java
+++ src/main/java/SubArrayProblems/RemoveShortestSubarray.java
@@ -1,4 +1,38 @@
 package SubArrayProblems;
 
 public class RemoveShortestSubarray {
+    public int findLengthOfShortestSubarray(int[] arr) {
+        int n = arr.length;
+
+        if(n == 0)
+            return 0;
+
+        int i=0, j=n-1;
+
+        while(i< n-1 && arr[i] <= arr[i+1]){
+            i++;
+        }
+
+        while(j > 0 && arr[j] > arr[j-1]){
+            j--;
+        }
+
+        int k=i;
+        i=0;
+
+        int smallestSubLength = Integer.MAX_VALUE;
+        while(i<= k){
+            if(arr[i] <= arr[j]){
+                smallestSubLength = Math.min(smallestSubLength, j-i-1);
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+
+        smallestSubLength = Math.min(smallestSubLength, j-i-1);
+
+        return smallestSubLength;
+    }
 }
